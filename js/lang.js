0ce206ebb779f1e2ec768b4d6e64c06e2b314bcd
𝗡𝗢𝗩𝗔-𝗫𝗠𝗗 <hanchobmb@gmail.com>
2025-11-17 09:40:43 +0300

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,114 @@
+// Language Translate
+function _0x40a5(){const _0x28bba0=['[data-translate]','forEach','englishText','arabicText','direction','DOMContentLoaded','getElementById','8708271DrVmYw','querySelectorAll','language','setItem','display','91751hIgqCE','rtl','style','8MrngfT','9JQOAbe','991389PNLHhB','12hpahDM','865960SpQsic','documentElement','getItem','dir','ltr','block','data-translate','613323fqVTaR','2UvQBGV','getAttribute','none','textContent','1358884vAkNlP','4978670jrDDMe'];
+                   const _0x56e1c3=_0x4e97;(function(_0x4608a9,_0x44c443){const _0x9e144d=_0x4e97,_0x708966=_0x4608a9(){try{const _0x4467d3=parseInt(_0x9e144d(0xa8))/0x1+parseInt(_0x9e144d(0x96))/0x2*(-parseInt(_0x9e144d(0x95))/0x3)
+                       +-parseInt(_0x9e144d(0x9a))/0x4+parseInt(_0x9e144d(0xaf))/0x5*(-parseInt(_0x9e144d(0xae))/0x6)+parseInt
+                       (_0x9e144d(0xad))/0x7*(-parseInt(_0x9e144d(0xab))/0x8)+parseInt(_0x9e144d(0xac))/0x9*(parseInt(_0x9e144d(0x9b))/0xa)+parseInt(_0x9e144d(0xa3))/0xb;
+            if(_0x4467d3===_0x44c443)break;else _0x708966['push'](_0x708966['shift']());}catch(_0x441e6c){_0x708966['push'](_0x708966['shift']());}}}(_0x40a5,0x553b2));currentDirection=localStorage[_0x56e1c3(0x90)](_0x56e1c3(0xa0))&&currentLanguage==='ar'?_0x56e1c3(0xa9):_0x56e1c3(0x92);function changeLanguage(_0x159f64){const _0x595a92=_0x56e1c3;currentLanguage=_0x159f64,localStorage(_0x595a92(0xa5),currentLanguage),currentDirection=currentLanguage==='ar'?_0x595a92(0xa9):_0x595a92(0x92),localStorage[_0x595a92(0xa6)](_0x595a92(0xa0),currentDirection),document[_0x595a92(0x8f)][_0x595a92(0x91)]=currentDirection,updatePageLanguage();}function updatePageLanguage(){const _0x506547=_0x56e1c3,_0x6f5686=document[_0x506547(0xa4)](_0x506547(0x9c));_0x6f5686[_0x506547(0x9d)](_0x226495=>{const _0x19a863=_0x506547,_0x18d289=_0x226495[_0x19a863(0x97)](_0x19a863(0x94));translations[currentLanguage][_0x18d289]&&(_0x226495[_0x19a863(0x99)]=translations[currentLanguage][_0x18d289]);const _0x4d5548=document[_0x506547(0xa2)](_0x506547(0x9e)),_0x1d8a00=document[_0x506547(0xa2)](_0x506547(0x9f));[_0x506547(0xaa)]['display']=_0x506547(0x98),_0x1d8a00[_0x506547(0xaa)][_0x506547(0xa7)]=_0x506547(0x93);else currentLanguage==='ar'&&(_0x4d5548[_0x506547(0xaa)][_0x506547(0xa7)]='block',_0x1d8a00[_0x506547(0xaa)][_0x506547(0xa7)]=_0x506547(0x98));}function _0x4e97(_0x438013,_0x4d09e4){const _0x40a589=_0x40a5();return _0x4e97=function(_0x4e97ed,_0x341b60){_0x4e97ed=_0x4e97ed-0x8f;let _0x23c3ce=_0x40a589[_0x4e97ed];return _0x23c3ce;},_0x4e97(_0x438013,_0x4d09e4);}document['addEventListener'](_0x56e1c3(0xa1),()=>{const _0x1bf0f4=_0x56e1c3;document['documentElement'][_0x1bf0f4(0x91)]=currentDirection,updatePageLanguage();});
+
+
+// English Language
+const translations = {
+    "en": {
+        // Navbar
+        "nav-home": "Home",
+        "nav-twit": "Twitter Downloader",
+        "nav-insta": "Instagram Downloader",
+        "nav-face": "Facebook Downloader",
+        "nav-you": "YouTube Downloader",
+
+        // Home
+        "home-title": "Website works in two languages, Arabic and English",
+        "home-title1": "Fast and free all in one video downloader",
+        "home-title2": "By",
+        "home-title3": "URL",
+        "home-btn": "Install App",
+
+        // About
+        "sec2-title1": "Supports The Most Popular Sources",
+        "sec2-title2": "You can check regularly updated supported sources list",
+        "sec2-btn1": "YouTube",
+        "sec2-btn2": "Facebook",
+        "sec2-btn3": "Instagram",
+        "sec2-btn4": "Twitter",
+
+        // How To Download
+        "sec3-title1": "How to Download Videos?",
+        "sec3-title2": "To download videos, you have to follow three easy steps:",
+
+        "sec3-num1": "1",
+        "sec3-title3": "Copy Link",
+        "sec3-title4": "Just copy link of source which you want to download",
+
+        "sec3-num2": "2",
+        "sec3-title5": "Paste Link",
+        "sec3-title6": "Paste a video link of supported sources into the input field above",
+
+        "sec3-num3": "3",
+        "sec3-title7": "Download Now",
+        "sec3-title8": "Click to download button and see files which is available to download",
+        "sec3-title9": "Download QR",
+
+
+        // Youtube Page
+        "tube-title": "Convert and download",
+        "tube-title2": "any YouTube video in MP4, MP3 and opus",
+        "twit-title2": "any Twitter video in MP4, MP3 and opus",
+        "insta-title2": "any Instagram video in MP4 and Images",
+        "face-title2": "any Facebook video in MP4, MP3 and opus",
+        "tube-btn": "Download",
+    },
+
+
+    // Arabic Language
+    "ar": {
+        // Navbar
+        "nav-home": "الصفحة الرئيسية",
+        "nav-twit": "تحميل من تويتر",
+        "nav-insta": "تحميل من الأنستجرام",
+        "nav-face": "تحميل من الفيس بوك",
+        "nav-you": "تحميل من اليوتيوب",
+
+        // Home
+        "home-title": "كما يعمل الموقع بلغتين العربية والانجليزية",
+        "home-title1": "السرعة والمجانية في فيديو واحد",
+        "home-title2": " بواسطة",
+        "home-title3": " لينك",
+        "home-btn": "تثبيت التطبيق",
+
+        // About
+        "sec2-title1": "يدعم أكثر من مصدر شائع",
+        "sec2-title2": "يمكنك التحقق من قائمة المصادر المدعومة المحدثة بانتظام",
+        "sec2-btn1": "اليوتيوب",
+        "sec2-btn2": "الفيس بوك",
+        "sec2-btn3": "الأنستجرام",
+        "sec2-btn4": "تويتر",
+
+        // How To Download
+        "sec3-title1": "كيفية تنزيل مقاطع الفيديو؟",
+        "sec3-title2": "لتنزيل مقاطع الفيديو، عليك اتباع ثلاث خطوات سهلة:",
+
+        "sec3-num1": "١",
+        "sec3-title3": "انسخ اللينك",
+        "sec3-title4": "فقط انسخ رابط المصدر الذي تريد تنزيله",
+
+        "sec3-num2": "٢",
+        "sec3-title5": "الصق اللينك",
+        "sec3-title6": "الصق رابط فيديو للمصادر المدعومة في حقل الإدخال",
+
+        "sec3-num3": "٣",
+        "sec3-title7": "التحميل الان",
+        "sec3-title8": "انقر فوق زر التنزيل وشاهد الملفات المتاحة للتنزيل",
+
+        "sec3-title9": "تحميل رمز الاستجابه",
+        
+
+        // Youtube Page
+        "tube-title": "تحويل وتنزيل",
+        "tube-title2": "أي فيديو يوتيوب بصيغة MP4 وMP3 وopus",
+        "twit-title2": "أي فيديو تويتر بصيغة MP4 وMP3 وopus",
+        "insta-title2": "أي فيديو انستجرام بصيغة MP4 و صور",
+        "face-title2": "أي فيديو فيس بوك بصيغة MP4 وMP3 وopus",
+        "tube-btn": "تحميل",
+    }
+};
+
